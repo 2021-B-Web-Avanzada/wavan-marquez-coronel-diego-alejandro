2e316faf3fb64d979cb04839306077b83a8ba57e
Diego Marquez <diego.marquez@epn.edu.ec>
2021-12-09 06:00:59 -0800

    update/angular routing

--- 03-angular/ejemplo/src/app/app.module.ts
+++ 03-angular/ejemplo/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RutaLoginComponent } from './ruta-login/ruta-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RutaLoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
